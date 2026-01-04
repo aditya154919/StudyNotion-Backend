75a27e7c6f7acdca19db13adb06e4b24e303df9b
Aditya Kumar <aditya154919@gmail.com>
2026-01-05 02:11:00 +0530

    fix cors issue

--- server.js
+++ server.js
@@ -11,10 +11,24 @@ const fileupload = require("express-fileupload")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://study-notion-frontend-zeta.vercel.app"
+];
+
 app.use(cors({
-    origin:"http://localhost:5173" || "https://study-notion-frontend-zeta.vercel.app/",
-    credentials:true
-}))
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 
 app.use(
     fileupload({
